a06a6a1467f1600e85ab1188c18901b202ac0956
annie <knr9144@icloud.com>
2022-07-13 16:45:05 +0900

    D-Day앱 storyboard 구현

---
+++ 2ndweekProject/DatePickerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DatePickerViewController.swift
+//  2ndweekProject
+//
+//  Created by 나리강 on 2022/07/13.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    @IBOutlet weak var thirdImageView: UIImageView!
+    @IBOutlet weak var fourthImageView: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+        
+    }
+    
+    
+    
+  
+
+}
