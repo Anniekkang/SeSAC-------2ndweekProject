24ca278a758dc8d4c2a6a85d02fff7ba6078feb5
annie <knr9144@icloud.com>
2022-07-13 18:37:45 +0900

    code로 외관 구현

--- 2ndweekProject/DatePickerViewController.swift
+++ 2ndweekProject/DatePickerViewController.swift
@@ -10,27 +10,24 @@ import UIKit
 class DatePickerViewController: UIViewController {
 
     
+    
     @IBOutlet weak var datePicker: UIDatePicker!
-    
-    @IBOutlet weak var firstImageView: UIImageView!
-    @IBOutlet weak var secondImageView: UIImageView!
-    @IBOutlet weak var thirdImageView: UIImageView!
-    @IBOutlet weak var fourthImageView: UIImageView!
-    
     @IBOutlet var imageViews : [UIImageView]!
-    
-    @IBOutlet weak var firstDdayLabel: UILabel!
-    @IBOutlet weak var secondDdayLabel: UILabel!
-    @IBOutlet weak var thirdDdayLabel: UILabel!
-    @IBOutlet weak var fourthDdayLabel: UILabel!
-   
     @IBOutlet var ddayLabels : [UILabel]!
+    @IBOutlet var dateLabels : [UILabel]!
+    
+    @IBOutlet weak var choiceButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         imageSetup()
         labelSetup()
+        
+        choiceButton.setTitle("날짜선택", for: .normal)
+        choiceButton.setTitleColor(.purple, for: .normal)
+        
+        
         
         
     }
@@ -44,20 +41,41 @@ class DatePickerViewController: UIViewController {
     }
     
     func labelSetup(){
-        firstDdayLabel.text = "D+100"
-        secondDdayLabel.text = "D+200"
-        thirdDdayLabel.text = "D+300"
-        fourthDdayLabel.text = "D+400"
-        
         for i in 0...3 {
           
             ddayLabels[i].font = UIFont.systemFont(ofSize: 20, weight: .heavy)
             ddayLabels[i].textColor = .white
+            ddayLabels[i].text = "D+\(100 * (i+1))"
         }
         
-        
-        
+        for i in 0...3 {
+            dateLabels[i].text = ""
+            dateLabels[i].textColor = .white
+            dateLabels[i].font = UIFont.systemFont(ofSize: 20, weight: .heavy)
+        }
+
     }
   
-
+    @IBAction func buttonAction(_ sender: UIButton) {
+        //dateLabel이 바뀐다 각 디데이에 맞춰서
+        //dateLabels[i] 0...3
+        
+        //choosen date
+        let pickedDate = Date()
+        let currentYear =  Calendar.current.dateComponents([.year], from: pickedDate)
+        let currentMonth = Calendar.current.dateComponents([.month], from: pickedDate)
+        let currentDay = Calendar.current.dateComponents([.day], from: pickedDate)
+        
+        
+        
+        
+        print(currentYear)
+        //D-Day
+//        for i in 1...3 {
+//            dateLabels[i] =
+//        }
+//
+        
+    }
+    
 }
