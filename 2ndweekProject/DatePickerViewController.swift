10db7f8268260cd8b259f33072333cf637a1917e
annie <knr9144@icloud.com>
2022-07-13 17:16:28 +0900

    storyboard 수정

--- 2ndweekProject/DatePickerViewController.swift
+++ 2ndweekProject/DatePickerViewController.swift
@@ -17,19 +17,47 @@ class DatePickerViewController: UIViewController {
     @IBOutlet weak var thirdImageView: UIImageView!
     @IBOutlet weak var fourthImageView: UIImageView!
     
+    @IBOutlet var imageViews : [UIImageView]!
     
+    @IBOutlet weak var firstDdayLabel: UILabel!
+    @IBOutlet weak var secondDdayLabel: UILabel!
+    @IBOutlet weak var thirdDdayLabel: UILabel!
+    @IBOutlet weak var fourthDdayLabel: UILabel!
+   
+    @IBOutlet var ddayLabels : [UILabel]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        
+        imageSetup()
+        labelSetup()
         
         
     }
     
+    func imageSetup(){
+        
+        for i in 0...3 {
+            imageViews[i].layer.cornerRadius = 20
+        }
+
+    }
     
-    
+    func labelSetup(){
+        firstDdayLabel.text = "D+100"
+        secondDdayLabel.text = "D+200"
+        thirdDdayLabel.text = "D+300"
+        fourthDdayLabel.text = "D+400"
+        
+        for i in 0...3 {
+          
+            ddayLabels[i].font = UIFont.systemFont(ofSize: 20, weight: .heavy)
+            ddayLabels[i].textColor = .white
+        }
+        
+        
+        
+    }
   
 
 }
